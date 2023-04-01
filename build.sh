9e00f817ba6e55b2e40df8c1ea3fea15006743e2
TomAwezome <sonidd1@yahoo.com>
2023-04-01 19:38:44 -0400

    Change TOC depth from 3 to 2.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 python3.11 md.py
-pandoc docs/xr17032.txt -V geometry:margin=1in --toc -o docs/xr17032.pdf
+pandoc docs/xr17032.txt -V geometry:margin=1in --toc --toc-depth=2 -o docs/xr17032.pdf
